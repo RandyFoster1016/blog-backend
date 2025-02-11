2af9f742ff85102405a1ef191994a1115f0ceb12
RandyFoster1016 <162823487+RandyFoster1016@users.noreply.github.com>
2025-02-11 08:56:23 -0500

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@
 const { Client } = require('pg');
 const client = new Client({
     user: 'randy',
-    host: 'postgresql://blogdb_ug6o_user:M3YIcQnHvR65WGCvCNFqy56qhAuhLpEn@dpg-cull7pl6l47c73dqloug-a/blogdb_ug6o',
+    host: 'localhost',
     database: 'blogdb',
     password: 'randy',
     port: 5432,
