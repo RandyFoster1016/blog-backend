0eb28314be20f4ad1840f502c57f5960c5b9965c
RandyFoster1016 <162823487+RandyFoster1016@users.noreply.github.com>
2025-02-12 09:52:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5432;
+const port = process.env.PORT || 8000;
 const client = require('./db/conn.js');
 const cors = require('cors');
 
